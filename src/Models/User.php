079642a4f867b108959efd53141d72c92c6cc555
Martin Vlcek <martin@nepropadejtepanice.com>
2017-05-18 13:06:40 +0200

    Password reset feature from pull request #999

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,11 +5,13 @@ namespace TCG\Voyager\Models;
 use Carbon\Carbon;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use TCG\Voyager\Contracts\User as UserContract;
+use Illuminate\Notifications\Notifiable;
+use TCG\Voyager\Notifications\ResetPassword;
 use TCG\Voyager\Traits\VoyagerUser;
 
 class User extends Authenticatable implements UserContract
 {
-    use VoyagerUser;
+    use Notifiable, VoyagerUser;
 
     protected $guarded = [];
 
@@ -20,6 +22,28 @@ class User extends Authenticatable implements UserContract
         }
 
         return $value;
+    }
+
+    /**
+     * On save make sure to set the default avatar if image is not set.
+     */
+    public function save(array $options = [])
+    {
+        // If no avatar has been set, set it to the default
+        $this->avatar = $this->avatar ?: config('voyager.user.default_avatar', 'users/default.png');
+
+        parent::save();
+    }
+
+    /**
+     * Send password reset notification
+     *
+     * @param string $token Password reset token
+     * @return void
+     */
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
     }
 
     public function setCreatedAtAttribute($value)
